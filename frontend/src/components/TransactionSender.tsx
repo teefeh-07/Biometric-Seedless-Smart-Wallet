0af345c43d462baf58a880cb1d2f55ff3cec9ce1
walterthesmart <nwaugowalter@gmail.com>
2025-12-20 06:21:49 +0100

    feat: define TransactionSender component

--- frontend/src/components/TransactionSender.tsx
+++ frontend/src/components/TransactionSender.tsx
@@ -1,2 +1,10 @@
 import React, { useState } from 'react';
 import { userSession } from '../lib/stacks-auth';
+
+export const TransactionSender = () => {
+    return (
+        <div>
+            <h3>Send Transaction</h3>
+        </div>
+    );
+};
