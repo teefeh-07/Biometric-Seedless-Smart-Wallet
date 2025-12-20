0af090d9c781e557337c151edde02a114a1d46ab
walterthesmart <nwaugowalter@gmail.com>
2025-12-20 06:25:30 +0100

    fix: use Pc instead of makeStandardSTXPostCondition

--- frontend/src/lib/stacks-tx/index.ts
+++ frontend/src/lib/stacks-tx/index.ts
@@ -4,7 +4,7 @@ import {
     PostConditionMode,
     makeContractCall,
     broadcastTransaction,
-    makeStandardSTXPostCondition,
+    Pc,
     FungibleConditionCode,
 } from '@stacks/transactions';
 
