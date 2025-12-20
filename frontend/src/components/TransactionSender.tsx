13086cd5f0eb90128a105f7ebbc9723314989567
walterthesmart <nwaugowalter@gmail.com>
2025-12-20 06:22:55 +0100

    feat: add tx handler

--- frontend/src/components/TransactionSender.tsx
+++ frontend/src/components/TransactionSender.tsx
@@ -3,10 +3,14 @@ import { userSession } from '../lib/stacks-auth';
 import { openContractCall } from '@stacks/connect';
 
 export const TransactionSender = () => {
+    const handleSend = () => {
+        // Logic
+    };
+
     return (
         <div>
             <h3>Send Transaction</h3>
-            <button>Send</button>
+            <button onClick={handleSend}>Send</button>
         </div>
     );
 };
